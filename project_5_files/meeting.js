7e188779c23538ca18da91fbc5a9bfe5fb1cc270
Kieran Bondy <k.bondmna7@gmail.com>
2019-02-19 18:47:27 -0600

    updating meeting

---
+++ project_5_files/meeting.js
@@ -0,0 +1,12 @@
+
+
+
+
+function dropdown_function(){
+  document.getElementById("dropdown_content").classList.toggle("show");
+}
+
+function onehr_function(){
+  document.getElementById("dropdown_content").classList.toggle("show");
+  document.getElementById("dropdown").placeholder = "1 hour";
+}
