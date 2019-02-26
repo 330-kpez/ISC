e6b527ab4101c7006da3595dd98f792e91230937
Patrice Power <patricepower2021@u.northwestern.edu>
2019-02-26 16:33:47 -0600

    link patrice.js lololololol

--- project_5_files/patrice.js
+++ project_5_files/patrice.js
@@ -4,18 +4,19 @@ var createbutt = document.getElementById("create")
 createbutt.onclick = submitForm1()
 submitbutt.onclick = submitForm2()
 
+var userName =
 
 function submitForm1() {
   document.getElementById("mySubmitForm").submit();
-  var userName = document.getElementById("UserName")
-  var email = document.getElementById("EmailAddr")
-  var password = document.getElementById("Password")
+  var userName = document.getElementById("UserName").value;
+  var email = document.getElementById("EmailAddr").value;
+  var password = document.getElementById("Password").value;
 }
 
 function submitForm2(){
   document.getElementById("mySubmitForm").submit();
-  var phone = document.getElementById("PhoneNum")
-  var hometown = document.getElementById("Hometown")
-  var edu = document.getElementById("Education")
-  var dob = document.getElementById("DOB")
+  var phone = document.getElementById("PhoneNum").value;
+  var hometown = document.getElementById("Hometown").value;
+  var edu = document.getElementById("Education").value;
+  var dob = document.getElementById("DOB").value;
 }
