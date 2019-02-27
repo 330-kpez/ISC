f7c394f6ea5a54a262659ec6c944020e4e3b60bc
Kieran Bondy <k.bondmna7@gmail.com>
2019-02-26 23:15:56 -0600

    ughhhhh took way too long

--- project_5_files/ellie.js
+++ project_5_files/ellie.js
@@ -6,16 +6,20 @@ var sw5 = 1;
 var sw6 = 1;
 var sw7 = 1;
 
+
 function d1_function(){
 
   if(sw1 == 0){
     document.getElementById("d1").style.background = "white";
     sw1 = 1;
+    document.getElementById("e1").style.textDecorationLine = "none";
   }
+
+
   else{
     document.getElementById("d1").style.background = "#77dbf4";
     sw1 = 0;
-
+    document.getElementById("e1").style.textDecorationLine = "line-through";
   }
 
 }
@@ -25,10 +29,12 @@ function d2_function(){
   if(sw2 == 0){
     document.getElementById("d2").style.background = "white";
     sw2 = 1;
+    document.getElementById("e2").style.textDecorationLine = "none";
   }
   else{
     document.getElementById("d2").style.background = "#77dbf4";
     sw2 = 0;
+    document.getElementById("e2").style.textDecorationLine = "line-through";
 
   }
 
@@ -39,10 +45,12 @@ function d3_function(){
   if(sw3 == 0){
     document.getElementById("d3").style.background = "white";
     sw3 = 1;
+    document.getElementById("e3").style.textDecorationLine = "none";
   }
   else{
     document.getElementById("d3").style.background = "#77dbf4";
     sw3 = 0;
+    document.getElementById("e3").style.textDecorationLine = "line-through";
 
   }
 
@@ -53,10 +61,12 @@ function d4_function(){
   if(sw4 == 0){
     document.getElementById("d4").style.background = "white";
     sw4 = 1;
+    document.getElementById("e4").style.textDecorationLine = "none";
   }
   else{
     document.getElementById("d4").style.background = "#77dbf4";
     sw4 = 0;
+    document.getElementById("e4").style.textDecorationLine = "line-through";
 
   }
 }
@@ -65,10 +75,12 @@ function d4_function(){
     if(sw5 == 0){
       document.getElementById("d5").style.background = "white";
       sw5 = 1;
+      document.getElementById("e5").style.textDecorationLine = "none";
     }
     else{
       document.getElementById("d5").style.background = "#77dbf4";
       sw5 = 0;
+      document.getElementById("e5").style.textDecorationLine = "line-through";
 
     }
 
